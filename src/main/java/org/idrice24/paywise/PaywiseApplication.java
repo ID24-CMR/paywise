897b60ccbaf3f6387f1c8f718110bd7eb66023b4
ID24-CMR <idrice.tsafouet@yahoo.com>
2024-12-17 08:28:14 +0100

    creating the projects files

---
+++ src/main/java/org/idrice24/paywise/PaywiseApplication.java
@@ -0,0 +1,13 @@
+package org.idrice24.paywise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaywiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaywiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/idrice24/paywise/PaywiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idrice24.paywise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaywiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
